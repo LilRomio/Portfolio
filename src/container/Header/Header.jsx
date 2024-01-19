9e0b083607902724e93a5d8e0b64642f10486b8e
LilRomio <popescu.romeo9@gmail.com>
2024-01-19 20:07:12 +0200

    bugs

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -50,13 +50,13 @@ const Header = () => {
         className="app__header-img"
       >
         <img src={images.profile} alt="profile_bg" />
-        <motion.img
+        {/* <motion.img
           whileInView={{ scale: [0, 1] }}
           transition={{ duration: 1, ease: 'easeInOut' }}
           src={images.circle}
           alt="profile-circle"
           className="overlay_circle"
-        />
+        /> */}
       </motion.div>
       <motion.div
         variant={scaleVariants}
